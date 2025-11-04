0dcb900a3fdbc897671e1506a128b85475252cac
cesar local <227235592+cgonzalesapps-code@users.noreply.github.com>
2025-11-03 20:10:13 -0500

    CargaProyectoV2

---
+++ prclientefinal/src/main/java/edu/cibertec/appConfig.java
@@ -0,0 +1,13 @@
+package edu.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class appConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(appConfig.class, args);
+	}
+
+}
---
+++ prclientefinal/src/main/java/edu/cibertec/controller/UsuarioController.java
@@ -0,0 +1,22 @@
+package edu.cibertec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("usuario")
+public class UsuarioController{
+
+    @RequestMapping("/")
+    public String loginMostrar(){
+        return "login";
+    }
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Controlador activo";
+    }
+}
+    
